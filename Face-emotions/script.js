ce37d83ea7aa0b861d9722863ee875d3dddb80bc
shiro-o <50459819+shiro-o@users.noreply.github.com>
2020-04-11 05:41:05 +0100

    Add files via upload

--- Face-emotions/script.js
+++ Face-emotions/script.js
@@ -2,7 +2,12 @@ const video = document.getElementById('video')
 
 console.log(video.width)
 
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('./models')
+]).then(startVideo);
 
+console.log(faceapi.nets)
 
 function startVideo() {
   navigator.getUserMedia(
@@ -12,69 +17,60 @@ function startVideo() {
   )
 
 
-
-    setTimeout( () => {
-//video.addEventListener('canplay', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
-
-
-
-  setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-   // faceapi.draw.drawDetections(canvas, resizedDetections)
-
-    //faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-
-
-
-      if(resizedDetections.length > 0) {
-          for(let i in resizedDetections)  {
-
-              const person = resizedDetections[i];
-
-              const box = person.detection.box;
-
-              const expressions = person.expressions;
-
-              var exp = 'sad';
-
-              for( const expression in expressions ) {
-                  if (expressions[expression] > expressions[exp]) {
-                      exp = expression;
-                  }
-              };
-
-              const drawOptions = {
-                  label: exp,
-                  lineWidth: 2,
-                  drawLabelOptions: {fontSize: 20}
-              }
-              const drawBox = new faceapi.draw.DrawBox(box, drawOptions)
-              drawBox.draw(canvas)
-
-          }
-
-      }
-
-
-  }, 200)
-
-}, 10000)
-
-
 }
 
 
+setTimeout( () => {
+//video.addEventListener('playing', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
 
 
-Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('./models')
-]).then(startVideo);
 
-console.log(faceapi.nets)
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceExpressions()
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        // faceapi.draw.drawDetections(canvas, resizedDetections)
+
+        //faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+
+
+
+        if(resizedDetections.length > 0) {
+            for(let i in resizedDetections)  {
+
+                const person = resizedDetections[i];
+
+                const box = person.detection.box;
+
+                const expressions = person.expressions;
+
+                var exp = 'sad';
+
+                for( const expression in expressions ) {
+                    if (expressions[expression] > expressions[exp]) {
+                        exp = expression;
+                    }
+                };
+
+                const drawOptions = {
+                    label: exp,
+                    lineWidth: 2,
+                    drawLabelOptions: {fontSize: 20}
+                }
+                const drawBox = new faceapi.draw.DrawBox(box, drawOptions)
+                drawBox.draw(canvas)
+
+            }
+
+        }
+
+
+    }, 200)
+
+}, 20000)
+
+
